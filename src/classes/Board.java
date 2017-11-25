f664f0a83b477d7b7523fc84ecef9e2239c756b9
WINDOWS-EMF9KVS\Tim <vandykti@mail.gvsu.edu>
2017-11-25 17:07:49 -0500

    Pushing some kinda broken skeleton code for ya

--- src/classes/Board.java
+++ src/classes/Board.java
@@ -1,6 +1,38 @@
 package classes;
 
 public class Board {
-	ThisIS A CHANGE
-	I CAN CHANGE THINGS TOO!!
+	private int board[][]; //the board
+	private int height;
+	private int width;
+	
+	
+	/*
+	 * the constructor
+	 * @param width is the width of the board
+	 * @param height is the height of the board
+	 */
+	public Board(int width, int height) {
+		this.height = height - 1; 
+		this.width = width - 1;
+		board = new int[width][height];
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public boolean isFull(int x, int y) {
+		boolean isFull;
+		if (board[x][y] != 0) {
+			isFull = true;
+		} else {
+			isFull = false;
+		}
+		return isFull;
+	}
+	
 } 
